0c768e8052b42140132570ba2e8c5c88a5b927ca
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-10-27 12:57:22 -0400

    solved four test cases

--- prison.js
+++ prison.js
@@ -1,40 +1,30 @@
 function prison(n, m, h, v) {
+
+    let x, y
+
+    if (typeof(h) == "object"){
+      x= h.length
+    }
+    else {
+      x= 1
+    }
+
+    if (typeof(v) == "object"){
+      y= v.length
+    }
+    else {
+      y= 1
+    }
+
+
     // Write your code here
-    let squaresInRow= []
-    let squaresInColumn= []
-    for (let i=0; i<n; i++){
-      squaresInRow.push(1)
-    }
-    for (let j=0; j<m; j++){
-      squaresInColumn.push(1)
-    }
-
-    let newsquaresInRow= []
-
-    for (let k=0; k<n; k++){
-      if (k != h) {
-        newsquaresInRow.push(1)
-      }
-      else if (k ==h) {
-        newsquaresInRow.push(2)
-      }
-    }
-
-    let newsquaresInColumn=[]
-
-    for (let l=0; l<n; l++){
-      if (l != v) {
-        newsquaresInColumn.push(1)
-      }
-      else {
-        newsquaresInColumn.push(2)
-      }
-    }
-
-    console.log(squaresInRow)
-    console.log(squaresInColumn)
-    console.log(newsquaresInRow)
-    console.log(newsquaresInColumn)
+    let hole= (x+1) * (y+1)
+    console.log(hole)
 }
 
 prison(3,3,2,2)
+
+// if you remove one h and one v
+// always results in 4 so (h.legth+1) * (v.length+1)
+// if you remove two h and two v
+// then results in 9 so (h.legth+1) * (v.length+1)
